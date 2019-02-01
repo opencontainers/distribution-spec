0e1a1e0877339ca5533fe52e10739b41c923d6d6
Atlas Kerr <atlaskerr@gmail.com>
2019-02-01 00:08:57 -0600

    schema: Set output to stdout.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- schema/gen.go
+++ schema/gen.go
@@ -14,4 +14,4 @@
 
 package schema
 
-//go:generate esc -private -o=fs.go -modtime=1546544639 -pkg=schema -include=.*\.json$ -ignore=test-fixtures/* .
+//go:generate esc -private -modtime=1546544639 -pkg=schema -include=.*\.json$ -ignore=test-fixtures/* .
