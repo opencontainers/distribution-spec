9a1888722670918cde2e11e9d2184ad05ee8cf46
Michael Glenn <mbg198618@gmail.com>
2021-06-13 04:41:21 -0500

    Create run.java.benchmark.java

---
+++ run.java.benchmark.java
@@ -0,0 +1,69 @@
+he following additional packages will be installed:
+binutils build-essential bzip2 ca-certificates comerr-dev cpp cpp
+-5 cr
+dbus dh-python distro-info-data dpkg-dev fakeroot file g++ 
+	9++-5
+	gcc
+girl.2
+	-glib
+	-2.0
+	git git-man ifupdown iproute2 isc-dhcp-client
+isc-dhcp-common iso-codes krb5-locales krb5-multidev less
+
+libalgorithm-diff-perl libalgorithm-diff-xs-perl libalgorithm-merge-p
+libapt-inst2.
+	0 
+	 libasn1-
+	8-
+	 libatm1 libatomicl libbsd0
+libbz2-
+	1.0 
+	 libc6-dev libcap-ng0 libcc1-
+	0 
+	
+libcurl3-gnutis libdbus-
+	1-3
+	libdbus-glib-
+	1-2
+	libdns-export162 libdpkg
+libedit2 liberror-perl libexpat1 libexpat1-dev libfakeroot libffi6
+libfile-fontllock-perl libgcc-
+	5
+	 libgdbm3 libgirepository-
+	1.0-1
+
+libglib2.
+	0-0 
+	libglib2.
+	0-
+	 libgmp10 libgnutls30 libgompl libgssapi
+libgssapi3-heimdal libgssrpc4 libhcrypto4-heimdal libheimbase1-heimdal
+libheimntlm0-heimdal libhogweed4 libhx509-
+5-
+	 libicu55 libidn11
+libisc-export160 libisl15 libitml libk5crypto3 libkadmScint-mit9
+libkadm5srv-mit9 libkdb5-
+	8 
+	 libkrb5-
+	26-
+	 libkrb5-
+	3
+
+libkrb5support0 libldap-
+	2.4-2
+	liblsan0 libmagic1 libmn10 libmpc3 libmp
+libmpfr4 libmpx0 libmysqlclient20 libnettle6 libpl1-kit0 libper15.22
+libpopt0 libpq5 libpython-all-dev libpython-dev libpython-stdlib
+libpython2. 7 libpython2. 7-dev libpython2. 7-minimal libpython2.7-stdli
+
+libpython3-stdlib libpython3.
+	5-
+	 libpython3.
+	5-
+	 libquadmath
+libregexp-common-perl libroken18-heimdal librtmp1 libsas12-2
+libsas12-modules libsas12-modules-db libsqlite3-0 libssl-dev libssl-do
+libssl1.0.0 libstdc+
+	+-5-dev libtasn1-6 libtsan0 libubsan0 libwind0-he
+libx11-6 libx11-data libxaul libxcb1 libxdmcp6 libxext6 libxml2 libxm
+libxtables11 linux-libc-dev Isb-release make manpages manpages-dev
