c9af7b4bf4dfc01ebc1dedd5c6dc1dcb0b3e6242
Brandon Mitchell <git@bmitch.net>
2025-10-25 12:23:19 -0400

    [WIP] Pass through repo and update gen with func options
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- conformance2/testdata.go
+++ conformance2/testdata.go
@@ -16,53 +16,106 @@ import (
 
 type testData struct {
 	name      string // name of data set for logs
-	repo      string // specifies the remote repository when the data has been pushed
 	tag       string // specifies the tag used for last manifest push
-	manifests map[digest.Digest][]byte
+	desc      map[digest.Digest]*descriptor
 	blobs     map[digest.Digest][]byte
+	manifests map[digest.Digest][]byte
 	manOrder  []digest.Digest // ordered list to push manifests, the last is optionally tagged
 }
 
 func newTestData(name, tag string) *testData {
 	return &testData{
 		name:      name,
-		manifests: map[digest.Digest][]byte{},
-		blobs:     map[digest.Digest][]byte{},
-		manOrder:  []digest.Digest{},
 		tag:       tag,
+		desc:      map[digest.Digest]*descriptor{},
+		blobs:     map[digest.Digest][]byte{},
+		manifests: map[digest.Digest][]byte{},
+		manOrder:  []digest.Digest{},
 	}
 }
 
-func (td *testData) genBlob(algo digest.Algorithm, size int64) (digest.Digest, []byte, error) {
+type genComp int
+
+const (
+	genCompUncomp genComp = iota
+	genCompGzip
+)
+
+type genOptS struct {
+	algo    digest.Algorithm
+	comp    genComp
+	subject *descriptor
+}
+
+type genOpt func(*genOptS)
+
+func genWithAlgo(algo digest.Algorithm) genOpt {
+	return func(opt *genOptS) {
+		opt.algo = algo
+	}
+}
+
+func genWithCompress(comp genComp) genOpt {
+	return func(opt *genOptS) {
+		opt.comp = comp
+	}
+}
+
+func genWithSubject(subject descriptor) genOpt {
+	return func(opt *genOptS) {
+		opt.subject = &subject
+	}
+}
+
+func (td *testData) genBlob(size int64, opts ...genOpt) (digest.Digest, []byte, error) {
+	gOpt := genOptS{
+		algo: digest.Canonical,
+	}
+	for _, opt := range opts {
+		opt(&gOpt)
+	}
 	b := make([]byte, size)
 	_, err := rand.Read(b)
 	if err != nil {
 		return digest.Digest(""), nil, err
 	}
-	dig := algo.FromBytes(b)
+	dig := gOpt.algo.FromBytes(b)
 	td.blobs[dig] = b
 	return dig, b, nil
 }
 
-// genLayer returns a new layer with:
+// genLayer returns a new layer containing a tar file returning:
 // - compressed digest
 // - uncompressed digest
-// - layer body (tar+gzip compressed)
-func (td *testData) genLayer(fileNum int) (digest.Digest, digest.Digest, []byte, error) {
-	buf := &bytes.Buffer{}
-	digUncomp := digest.Canonical.Digester()
-	digComp := digest.Canonical.Digester()
-	mwComp := io.MultiWriter(buf, digComp.Hash())
-	gw := gzip.NewWriter(mwComp)
-	mwUncomp := io.MultiWriter(gw, digUncomp.Hash())
-	tw := tar.NewWriter(mwUncomp)
+// - layer body (tar+compression)
+func (td *testData) genLayer(fileNum int, opts ...genOpt) (digest.Digest, digest.Digest, []byte, error) {
+	gOpt := genOptS{
+		comp: genCompGzip,
+		algo: digest.Canonical,
+	}
+	for _, opt := range opts {
+		opt(&gOpt)
+	}
+	bufUncomp := &bytes.Buffer{}
+	bufComp := &bytes.Buffer{}
+	var wUncomp io.Writer
+	var mt string
+	switch gOpt.comp {
+	case genCompGzip:
+		wUncomp = gzip.NewWriter(bufComp)
+		mt = "application/vnd.oci.image.layer.v1.tar+gzip"
+	case genCompUncomp:
+		wUncomp = bufComp
+		mt = "application/vnd.oci.image.layer.v1.tar"
+	}
+	wTar := tar.NewWriter(wUncomp)
 	bigRandNum, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
 	if err != nil {
 		return digest.Digest(""), digest.Digest(""), nil, err
 	}
 	randNum := bigRandNum.Int64()
 	file := fmt.Sprintf("Conformance test file contents for file number %d.\nTodays lucky number is %d\n", fileNum, randNum)
-	err = tw.WriteHeader(&tar.Header{
+	err = wTar.WriteHeader(&tar.Header{
 		Name: fmt.Sprintf("./conformance-%d.txt", fileNum),
 		Size: int64(len(file)),
 		Mode: 0644,
@@ -70,25 +123,46 @@ func (td *testData) genLayer(fileNum int) (digest.Digest, digest.Digest, []byte,
 	if err != nil {
 		return digest.Digest(""), digest.Digest(""), nil, err
 	}
-	_, err = tw.Write([]byte(file))
+	_, err = wTar.Write([]byte(file))
 	if err != nil {
 		return digest.Digest(""), digest.Digest(""), nil, err
 	}
-	err = tw.Close()
+	err = wTar.Close()
 	if err != nil {
 		return digest.Digest(""), digest.Digest(""), nil, err
 	}
-	err = gw.Close()
+	if closer, ok := wUncomp.(io.Closer); gOpt.comp != genCompUncomp && ok {
+		err = closer.Close()
+	}
 	if err != nil {
 		return digest.Digest(""), digest.Digest(""), nil, err
 	}
-	body := buf.Bytes()
-	td.blobs[digComp.Digest()] = body
-	return digComp.Digest(), digUncomp.Digest(), body, nil
+	bodyComp := bufComp.Bytes()
+	bodyUncomp := bufUncomp.Bytes()
+	digComp := gOpt.algo.FromBytes(bodyComp)
+	digUncomp := gOpt.algo.FromBytes(bodyUncomp)
+	td.blobs[digComp] = bodyComp
+	td.desc[digComp] = &descriptor{
+		MediaType: mt,
+		Digest:    digComp,
+		Size:      int64(len(bodyComp)),
+	}
+	td.desc[digUncomp] = &descriptor{
+		MediaType: "application/vnd.oci.image.layer.v1.tar",
+		Digest:    digUncomp,
+		Size:      int64(len(bodyUncomp)),
+	}
+	return digComp, digUncomp, bodyComp, nil
 }
 
 // genConfig returns a config for the given platform and list of uncompressed layer digests.
-func (td *testData) genConfig(p platform, layers []digest.Digest) (digest.Digest, []byte, error) {
+func (td *testData) genConfig(p platform, layers []digest.Digest, opts ...genOpt) (digest.Digest, []byte, error) {
+	gOpt := genOptS{
+		algo: digest.Canonical,
+	}
+	for _, opt := range opts {
+		opt(&gOpt)
+	}
 	config := image{
 		Author:   "OCI Conformance Test",
 		platform: p,
@@ -101,13 +175,19 @@ func (td *testData) genConfig(p platform, layers []digest.Digest) (digest.Digest
 	if err != nil {
 		return digest.Digest(""), nil, err
 	}
-	dig := digest.Canonical.FromBytes(body)
+	dig := gOpt.algo.FromBytes(body)
 	td.blobs[dig] = body
 	return dig, body, nil
 }
 
 // genManifest returns an image manifest with the selected config and compressed layer digests.
-func (td *testData) genManifest(conf digest.Digest, layers []digest.Digest) (digest.Digest, []byte, error) {
+func (td *testData) genManifest(conf digest.Digest, layers []digest.Digest, opts ...genOpt) (digest.Digest, []byte, error) {
+	gOpt := genOptS{
+		algo: digest.Canonical,
+	}
+	for _, opt := range opts {
+		opt(&gOpt)
+	}
 	m := manifest{
 		SchemaVersion: 2,
 		MediaType:     "application/vnd.oci.image.manifest.v1+json",
@@ -116,7 +196,8 @@ func (td *testData) genManifest(conf digest.Digest, layers []digest.Digest) (dig
 			Digest:    conf,
 			Size:      int64(len(td.blobs[conf])),
 		},
-		Layers: make([]descriptor, len(layers)),
+		Layers:  make([]descriptor, len(layers)),
+		Subject: gOpt.subject,
 	}
 	for i, l := range layers {
 		m.Layers[i] = descriptor{
@@ -129,14 +210,25 @@ func (td *testData) genManifest(conf digest.Digest, layers []digest.Digest) (dig
 	if err != nil {
 		return digest.Digest(""), nil, err
 	}
-	dig := digest.Canonical.FromBytes(body)
+	dig := gOpt.algo.FromBytes(body)
 	td.manifests[dig] = body
 	td.manOrder = append(td.manOrder, dig)
+	td.desc[dig] = &descriptor{
+		MediaType: "application/vnd.oci.image.manifest.v1+json",
+		Digest:    dig,
+		Size:      int64(len(body)),
+	}
 	return dig, body, nil
 }
 
 // genIndex returns an index manifest with the specified layers and platforms.
-func (td *testData) genIndex(platforms []*platform, manifests []digest.Digest) (digest.Digest, []byte, error) {
+func (td *testData) genIndex(platforms []*platform, manifests []digest.Digest, opts ...genOpt) (digest.Digest, []byte, error) {
+	gOpt := genOptS{
+		algo: digest.Canonical,
+	}
+	for _, opt := range opts {
+		opt(&gOpt)
+	}
 	if len(platforms) != len(manifests) {
 		return digest.Digest(""), nil, fmt.Errorf("genIndex requires the same number of platforms and layers")
 	}
@@ -144,6 +236,7 @@ func (td *testData) genIndex(platforms []*platform, manifests []digest.Digest) (
 		SchemaVersion: 2,
 		MediaType:     "application/vnd.oci.image.index.v1+json",
 		Manifests:     make([]descriptor, len(manifests)),
+		Subject:       gOpt.subject,
 	}
 	for i, l := range manifests {
 		ind.Manifests[i] = descriptor{
@@ -157,8 +250,13 @@ func (td *testData) genIndex(platforms []*platform, manifests []digest.Digest) (
 	if err != nil {
 		return digest.Digest(""), nil, err
 	}
-	dig := digest.Canonical.FromBytes(body)
+	dig := gOpt.algo.FromBytes(body)
 	td.manifests[dig] = body
 	td.manOrder = append(td.manOrder, dig)
+	td.desc[dig] = &descriptor{
+		MediaType: "application/vnd.oci.image.index.v1+json",
+		Digest:    dig,
+		Size:      int64(len(body)),
+	}
 	return dig, body, nil
 }
